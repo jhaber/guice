404856d5cc468bc5e65b6efc9a4a750ac02016c3
Jonathan Haber <jhaber@hubspot.com>
2020-04-08 13:49:13 -0400

    Update UntargettedBindingProcessor.java

--- core/src/com/google/inject/internal/UntargettedBindingProcessor.java
+++ core/src/com/google/inject/internal/UntargettedBindingProcessor.java
@@ -33,39 +33,46 @@ class UntargettedBindingProcessor extends AbstractBindingProcessor {
   @Override
   public <T> Boolean visit(Binding<T> binding) {
     return binding.acceptTargetVisitor(
-        new Processor<T, Boolean>((BindingImpl<T>) binding) {
-          @Override
-          public Boolean visit(UntargettedBinding<? extends T> untargetted) {
-            prepareBinding();
+      new Processor<T, Boolean>((BindingImpl<T>) binding) {
 
-            // Error: Missing implementation.
-            // Example: bind(Date.class).annotatedWith(Red.class);
-            // We can't assume abstract types aren't injectable. They may have an
-            // @ImplementedBy annotation or something.
-            if (key.getAnnotationType() != null) {
-              errors.missingImplementationWithHint(key, injector);
-              putBinding(invalidBinding(injector, key, source));
-              return true;
-            }
+        @Override
+        public Boolean visit(UntargettedBinding<? extends T> untargetted) {
+          prepareBinding();
 
-            // This cast is safe after the preceeding check.
-            try {
-              BindingImpl<T> binding =
-                  injector.createUninitializedBinding(key, scoping, source, errors, false);
-              scheduleInitialization(binding);
-              putBinding(binding);
-            } catch (ErrorsException e) {
-              errors.merge(e.getErrors());
-              putBinding(invalidBinding(injector, key, source));
-            }
-
+          // Error: Missing implementation.
+          // Example: bind(Date.class).annotatedWith(Red.class);
+          // We can't assume abstract types aren't injectable. They may have an
+          // @ImplementedBy annotation or something.
+          if (key.getAnnotationType() != null) {
+            errors.missingImplementationWithHint(key, injector);
+            putBinding(invalidBinding(injector, key, source));
             return true;
           }
 
-          @Override
-          protected Boolean visitOther(Binding<? extends T> binding) {
-            return false;
+          // This cast is safe after the preceeding check.
+          try {
+            BindingImpl<T> binding = injector.createUninitializedBinding(
+              key,
+              scoping,
+              source,
+              errors,
+              false
+            );
+            scheduleInitialization(binding);
+            putBinding(binding);
+          } catch (ErrorsException e) {
+            errors.merge(e.getErrors());
+            putBinding(invalidBinding(injector, key, source));
           }
-        });
+
+          return true;
+        }
+
+        @Override
+        protected Boolean visitOther(Binding<? extends T> binding) {
+          return false;
+        }
+      }
+    );
   }
 }
